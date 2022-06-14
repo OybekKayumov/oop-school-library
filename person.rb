b9d23414db83672c9e3a07ffd367ad123bb76fec
Oybek Kayumov <oybek.kayumov@gmail.com>
2022-06-14 19:46:22 +0500

    add decorate class

--- person.rb
+++ person.rb
@@ -1,12 +1,18 @@
-class Person
+require_relative './nameable'
+class Person < Nameable
   attr_reader :id
-  attr_accessor :name, age
+  attr_accessor :name, :age
 
   def initialize(_id, age, name = 'Unknown', parent_permission: true)
+    super()
     @id = Random.rand(1..1000)
     @name = name
     @age = age
     @parent_permission = parent_permission
+  end
+
+  def correct_name
+    @name
   end
 
   def can_use_services?
@@ -17,5 +23,18 @@ class Person
 
   def of_age?
     @age >= 18
+  end
+end
+
+class Decorator < Nameable
+  attr_accessor :nameable
+
+  def initialize(nameable)
+    super()
+    @nameable = nameable
+  end
+
+  def correct_name
+    @nameable.correct_name
   end
 end
