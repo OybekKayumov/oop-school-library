18f22d6b8e9a9bf7ca5651db1b088080e36dc969
Oybek Kayumov <oybek.kayumov@gmail.com>
2022-06-15 17:40:00 +0500

    add classroom class

---
+++ classroom.rb
@@ -0,0 +1,14 @@
+class Classroom
+  attr_reader :students
+  attr_accessor :label
+
+  def initialize(label)
+    @label = label
+    @student = []
+  end
+
+  def add_student(student)
+    @student.push(student)
+    student.classroom = self    
+  end  
+end
