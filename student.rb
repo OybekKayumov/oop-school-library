c69065bd448de48cec70f7514e5a80090d3bdd75
Oybek Kayumov <oybek.kayumov@gmail.com>
2022-06-17 05:42:47 +0500

    update rental, student, teacher classes

--- student.rb
+++ student.rb
@@ -3,8 +3,8 @@ require_relative './person'
 class Student < Person
   attr_reader :classroom
 
-  def initialize(id, age, classroom, name = 'Unknown', parent_permission: true)
-    super(id, age, name, parent_permission: parent_permission)
+  def initialize(age:, name:, parent_permission:, classroom: nil)
+    super(age: age, name: name, parent_permission: parent_permission)
     @classroom = classroom
   end
 
